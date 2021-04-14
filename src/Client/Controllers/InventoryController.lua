14fa89679dae9205b77e073e520a566e3a3460ea
Dodo12w <79851069+Dodo12w@users.noreply.github.com>
2021-04-13 20:41:50 -0400

    editbranch

--- src/Client/Controllers/InventoryController.lua
+++ src/Client/Controllers/InventoryController.lua
@@ -8,21 +8,21 @@ local InventoryController = {}
 
 function InventoryController:Start()
 
-    local InventoryModule = self.Modules.Inventory
+    local InventoryModule = self.Modules.Inventory --Getting the Inventory Module
 
-    self.Services.DataStoreService.SendInv:Connect(function(inventory)
+    self.Services.DataStoreService.SendInv:Connect(function(inventory) --Connecting the inventory event
 
-        local player = game.Players.LocalPlayer
+        local player = game.Players.LocalPlayer --Getting the player
 
-        repeat wait() until player.Character
+        repeat wait() until player.Character --Waiting for the character to load for replication
 
         local InventoryScreen = player.PlayerGui["Inventory 2.0"]
         local Inventory = InventoryScreen["Overall Frame"].Inventoryframe.Inventory
         local defaultitem = Inventory.Itemframe
         
-        for index, item in ipairs(inventory) do
+        for index, item in ipairs(inventory) do --Looping through inventory argument passed from the server
 
-            InventoryModule:LoadItem(item, player)
+            InventoryModule:LoadItem(item, player) --Calling the Inventory Module to create an item gui for the item in the inventory
         
         end
 	
