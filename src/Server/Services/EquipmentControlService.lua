269ef62050a678a7dbd7f1294c590a5c77db0fab
Dodo12w <79851069+Dodo12w@users.noreply.github.com>
2021-05-13 21:15:00 -0400

    this too on the editbranch

---
+++ src/Server/Services/EquipmentControlService.lua
@@ -0,0 +1,27 @@
+-- Equipment Control Service
+-- Username
+-- May 13, 2021
+
+
+
+local EquipmentControlService = {Client = {}}
+
+
+function EquipmentControlService.Client:LoadAsseccory(AsseccoryName)
+
+    print("The server should put the asseccory on the player...")
+
+end
+
+
+function EquipmentControlService:Start()
+	
+end
+
+
+function EquipmentControlService:Init()
+	
+end
+
+
+return EquipmentControlService
